059f385a5721055f7fa7f63b249cb16f242e4a09
dinomite505 <dino.milje@gmail.com>
2024-01-17 11:04:59 +0100

    Debugged payload and added undefined replacer

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ try {
     console.log(`Hello ${nameToGreet}!`);
     const time = (new Date()).toTimeString();
     core.setOutput("time", time);
-    // 
-    const payload = JSON.stringify(github.context.payload, 2)
+    //
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
     console.log(`The event payload: ${payload}`);
 }   catch (error) {
     
