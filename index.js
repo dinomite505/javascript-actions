653e0b4f0dd75605d185086853d06b5cf5adcfb5
dinomite505 <dino.milje@gmail.com>
2024-01-17 11:11:25 +0100

    Set exit code for actions

--- index.js
+++ index.js
@@ -8,9 +8,10 @@ try {
     console.log(`Hello ${nameToGreet}!`);
     const time = (new Date()).toTimeString();
     core.setOutput("time", time);
-    //
+    // This gets the JSON webhook for the event that triggered the workflow
     const payload = JSON.stringify(github.context.payload, undefined, 2)
     console.log(`The event payload: ${payload}`);
 }   catch (error) {
-    
+    // Refers to actions/core module. Provided error message will be displayed in the Actions logs
+    core.setFailed(error.message);
 }
